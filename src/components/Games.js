7e0ca5af7503cb9a1c71383de3dd4594e36fe2b5
Veysel Mustafa Nantu <veulerv@gmail.com>
2021-12-24 13:59:00 +0300

    player info update

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,18 +1,34 @@
-import { useEffect,useState } from "react";
-import {useLocation} from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import "./gamespage.css";
 function Games() {
-    const [gameList, setGameList] = useState([]);
-    const { state } = useLocation();
-    console.log("geçti",state);
-  useEffect (() => {
+  const [gameList, setGameList] = useState([]);
+  const { state } = useLocation();
+  console.log("geçti", state);
+  useEffect(() => {
     const gameListreq = fetch("http://localhost:3001/games")
-    .then((res) => res.json())
-    .then((result) => setGameList(result));
-  }, [])
-  
+      .then((res) => res.json())
+      .then((result) => setGameList(result));
+  }, []);
 
   console.log(gameList);
-  return <div>selam</div>;
+  return (
+    <div className="gamesmainwrapper">
+        <img
+        className="avatar"
+        src={require("./" + state.avatar)} />
+        <div
+        className="playerinfo-desc">
+          <div
+          className="playername">{state.name}</div>
+          <br />
+          <div
+          className="playerdesc">{state.event}</div>
+          
+        </div>
+      
+    </div>
+  );
 }
 
 export default Games;
